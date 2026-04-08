982ed89f8776c3198ea95d5c64282ddce2c38f28
Abid Azam Khan <abidazamkhan26@gmail.com>
2026-04-08 07:45:01 +0600

    feat: hero section component

---
+++ components/Hero.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="section slide1 p-0" id="home">
+      <div className="slider-area" id="slider-area">
+        <div className="row align-items-center">
+          <div className="col-12 col-lg-6 image-order">
+            <div className="slider-image">
+              <img src="/personal-hotspot/images/slider-img.png" alt="Abid Azam Khan Portfolio" />
+            </div>
+          </div>
+          <div className="col-12 col-lg-6 text-md-center text-lg-left">
+            <div className="slider-content">
+              <h1 className="main-font text-uppercase">
+                <span className="slider-text px-2"> Hello, I am </span> 
+                <span className="d-block text-yellow"> Abid Azam Khan.</span>
+              </h1>
+              <p className="pt-3 alt-font content-para">
+                Specialized in UI/UX, Branding and digital designs. 12+ years of experience in creating premium digital experiences.
+              </p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="circle-one">
+          <span data-tootik="About" data-tootik-conf="right dark square shadow">
+            <a href="#about">
+              <span className="animated-circle position-relative"></span>
+            </a>
+          </span>
+        </div>
+        
+        <div className="circle-two">
+          <span data-tootik="Timeline" data-tootik-conf="left dark square shadow">
+            <a href="#timeline">
+              <span className="animated-circle position-relative"></span>
+            </a>
+          </span>
+        </div>
+        
+        <div className="circle-three">
+          <span data-tootik="Portfolio" data-tootik-conf="top dark square shadow">
+            <a href="#portfolio">
+              <span className="animated-circle position-relative"></span>
+            </a>
+          </span>
+        </div>
+        
+        <div className="circle-four">
+          <span data-tootik="Testimonials" data-tootik-conf="bottom dark square shadow">
+            <a href="#testimonial">
+              <span className="animated-circle position-relative"></span>
+            </a>
+          </span>
+        </div>
+        
+        <div className="circle-five">
+          <span data-tootik="Contact" data-tootik-conf="bottom dark square shadow">
+            <a href="#contact">
+              <span className="animated-circle position-relative"></span>
+            </a>
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
