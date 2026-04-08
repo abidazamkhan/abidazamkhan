52ebee632ea9236f16a755f9ac3f825107510611
Abid Azam Khan <abidazamkhan26@gmail.com>
2026-04-08 07:45:08 +0600

    feat: footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="slider-bottom">
+      <div className="slider-footer w-auto">
+        <ul className="social-icons-simple">
+          <li><a className="social-icon" href="#"><i className="fab fa-facebook-f"></i> </a> </li>
+          <li><a className="social-icon" href="#"><i className="fab fa-instagram"></i> </a> </li>
+          <li><a className="social-icon" href="#"><i className="fab fa-x-twitter"></i> </a> </li>
+          <li><a className="social-icon" href="#"><i className="fab fa-linkedin-in"></i> </a> </li>
+        </ul>
+      </div>
+      <span className="slider-copyright text-grey">© MegaOne 2024.</span>
+    </div>
+  );
+};
+
+export default Footer;
